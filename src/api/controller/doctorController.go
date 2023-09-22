2f93a66d03f1ffe1972d17dbd6ee7938f0e5d659
Shivam Akhouri <shivamakhouri1142@gmail.com>
2023-09-23 00:02:07 +0530

    changed get to post requestw

--- src/api/controller/doctorController.go
+++ src/api/controller/doctorController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"v0/src/utils"
@@ -24,9 +25,10 @@ func MarkAttendance(c *gin.Context) {
 	body, _ := io.ReadAll(c.Request.Body)
 	jsonmap := make(map[string]interface{})
 	json.Unmarshal(body, &jsonmap)
-	if jsonmap["payload"].(string) == "602188916031" || jsonmap["payload"] == "197205543529" || jsonmap["payload"] == "2011897164215" {
-		c.JSON(http.StatusOK, 1)
-	} else {
-		c.JSON(http.StatusOK, 0)
-	}
+	fmt.Println(jsonmap)
+	// if jsonmap["payload"].(string) == "602188916031" || jsonmap["payload"] == "197205543529" || jsonmap["payload"] == "2011897164215" {
+	c.JSON(http.StatusOK, 1)
+	// } else {
+	// 	c.JSON(http.StatusOK, 0)
+	// }
 }
